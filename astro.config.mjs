e235919ced434ae9c4f8ec2103b78c3a7dbf7748
Anil Verma <anilcs240@gmail.com>
2023-10-08 15:35:51 +0530

    enabled server side rendering and dynamic routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
+import nodejs from '@astrojs/node';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: nodejs({
+    mode: 'middleware' // or 'standalone'
+  }),
   integrations: [tailwind()]
 });
